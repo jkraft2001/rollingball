250a4e15af9f77e7e4d51ab36c0fcb1863d7a313
JKrafty <89028895+jkraft2001@users.noreply.github.com>
2022-09-12 12:57:18 -0500

    Add files via upload

---
+++ BallRoll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRoll : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody myRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        speed = 6;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var horzMove = Input.GetAxis("Horizontal");
+        var vertMove = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horzMove, 0, vertMove);
+
+        var relativeMovement = Camera.main.transform.TransformVector(movement);
+
+        myRB.AddForce(relativeMovement * speed);
+    }
+}
